53a5f45e368775026113c9c0916a6a507978c279
Kapil gole <kapilprajapat0011@gmail.com>
2025-05-29 22:33:55 +0530

    ADD YAML

--- main.tf
+++ main.tf
@@ -213,3 +213,27 @@ resource "aws_eks_node_group" "public_nodes" {
     aws_iam_role_policy_attachment.ecr_policy
   ]
 }
+
+# Node Group in Private Subnet (for web traffic)
+resource "aws_eks_node_group" "private_nodes" {
+  cluster_name    = aws_eks_cluster.eks.name
+  node_group_name = "private-node-group"
+  node_role_arn   = aws_iam_role.eks_node_role.arn
+
+  subnet_ids = [aws_subnet.private.id]
+
+  scaling_config {
+    desired_size = 1
+    max_size     = 3
+    min_size     = 1
+  }
+  instance_types = ["t3.medium"]
+  capacity_type = "ON_DEMAND"
+
+  depends_on = [
+    aws_eks_cluster.eks,
+    aws_iam_role_policy_attachment.node_policy,
+    aws_iam_role_policy_attachment.cni_policy,
+    aws_iam_role_policy_attachment.ecr_policy
+  ]
+}
